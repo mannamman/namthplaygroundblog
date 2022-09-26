b5d254c689a111883a98f3a624cd22234cbdcf56
namth <wase894@gmail.com>
2022-09-26 15:40:58 +0900

    fix: 경로 문제 수정

--- project/local_to_gcp_storage_v3/file_worker.py
+++ project/local_to_gcp_storage_v3/file_worker.py
@@ -27,8 +27,6 @@ class Worker:
         # 로컬 경로 및 스토리지 경로 설정
         self.local_base_path = os.getenv("local_base_path")
         self.len_local_base_path = len(self.local_base_path) + 1
-        self.storage_base_path = os.getenv("storage_base_path")
-        self.len_storage_base_path = len(self.storage_base_path) + 1
         # 스토리지 객체 생성
         self.client = self._load_cred()
         self.bucket = self.client.get_bucket(os.getenv("gooogle_bucket_name"))
@@ -64,7 +62,7 @@ class Worker:
     def get_storage_hash(self, storage_file_list: List[str]) -> Dict[str,str]:
         storage_hash_dict = {}
         for storage_file in storage_file_list:
-            blob_name = f"{self.storage_base_path}/{storage_file}"
+            blob_name = storage_file
             blob = self.bucket.get_blob(blob_name)
             blob.reload()
             storage_hash_dict[storage_file] = binascii.hexlify(base64.urlsafe_b64decode(blob.md5_hash)).decode("utf-8")
@@ -72,7 +70,7 @@ class Worker:
 
     def delete_storage_files(self, thread_idx: int, file_list: List[str], pbar: tqdm):
         for del_f_path in file_list:
-            blob_name = f"{self.storage_base_path}/{del_f_path}"
+            blob_name = del_f_path
             blob = self.bucket.get_blob(blob_name)
             blob.delete()
             pbar.update(1)
@@ -80,7 +78,7 @@ class Worker:
     
     def upload_files(self, thread_idx: int, file_list: List[str], pbar: tqdm):
         for upload_f_path in file_list:
-            blob_name = f"{self.storage_base_path}/{upload_f_path}"
+            blob_name = upload_f_path
             mime = self._get_content_type(upload_f_path)
             local_full_path = f"{self.local_base_path}/{upload_f_path}"
             blob = self.bucket.blob(blob_name)
@@ -92,11 +90,10 @@ class Worker:
             pbar.write(f"{thread_idx} Thread Upload Done, {local_full_path}")
             
     def download_files(self, thread_idx: int, file_list: List[str], pbar: tqdm):
-        storage_base_path = self.storage_base_path
         local_base_path = self.local_base_path
         for download_f_path in file_list:
             local_full_path = f"{local_base_path}/{download_f_path}"
-            blob_name = f"{storage_base_path}/{download_f_path}"
+            blob_name = download_f_path
             blob = self.bucket.blob(blob_name)
             with open(local_full_path, "wb") as f:
                 self.client.download_blob_to_file(blob, f)
@@ -126,14 +123,14 @@ class Worker:
         file_list = list()
         blobs: List[Blob] = list(self.bucket.list_blobs(prefix=target))
         for blob in blobs:
-            f_name = blob.name[self.len_storage_base_path:]
+            f_name = blob.name
             if(f_name != ""):
                 file_list.append(f_name)
         return file_list
 
     def get_file_list(self, target: str) -> Tuple[List[str], List[str]]:
         # 스토리지 파일리스트 가져오기
-        storage_target_path = f"{self.storage_base_path}/{target}"
+        storage_target_path = target
         storage_file_list = self._get_storage_file_list(storage_target_path)
 
         # 로컬 파일리스트 가져오기
